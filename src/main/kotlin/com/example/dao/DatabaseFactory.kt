d9356bf8d7787b2153b8142c73a9088df98d21f1
vjimcor955 <vjimcor955@g.educaand.es>
2023-05-12 09:22:09 +0200

    ktor y exposed funciona

---
+++ src/main/kotlin/com/example/dao/DatabaseFactory.kt
@@ -0,0 +1,23 @@
+package com.example.dao
+
+import com.example.models.*
+import kotlinx.coroutines.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.*
+import org.jetbrains.exposed.sql.transactions.experimental.*
+
+object DatabaseFactory {
+    fun init() {
+        val driverClassName = "org.h2.Driver"
+        val jdbcURL = "jdbc:h2:file:./build/db"
+        val user = "admin"
+        val password = "admin"
+        val database = Database.connect(jdbcURL, driverClassName, user, password)
+        transaction(database) {
+            SchemaUtils.create(Articles)
+        }
+    }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
---
+++ src/main/resources/templates/_layout.ftl
@@ -0,0 +1,17 @@
+<#macro header>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Kotlin Journal</title>
+    </head>
+    <body style="text-align: center; font-family: sans-serif">
+<#--    <img src="/static/ktor_logo.png" alt="Logo de Ktor">-->
+    <img src="https://raw.githubusercontent.com/ktorio/ktor-documentation/2.3.0/codeSnippets/snippets/tutorial-website-static/src/main/resources/files/ktor_logo.png" alt="Logo de Kator">
+    <h1>Kotlin Ktor Journal </h1>
+    <p><i>Powered by Ktor & Freemarker!</i></p>
+    <hr>
+    <#nested>
+    <a href="/">Back to the main page</a>
+    </body>
+    </html>
+</#macro>
